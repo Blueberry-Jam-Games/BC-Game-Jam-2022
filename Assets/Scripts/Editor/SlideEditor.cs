75b079e881a413a87c50f90dc8b3f981959634dc
Quinn <quinnrichardson91@gmail.com>
2022-09-17 17:56:29 -0700

    .

---
+++ Assets/Scripts/Editor/SlideEditor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RuntimeSlide))]
+public class RuntimeSlideInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        RuntimeSlide slide = (RuntimeSlide)target;
+
+        if (GUILayout.Button("Add staff"))
+        {
+            slide.addStaff();
+        }
+
+        if (GUILayout.Button("Remove staff"))
+        {
+            slide.removeStaff();
+        }
+
+        if (GUILayout.Button("Close SLide"))
+        {
+            slide.closeRide();
+        }
+    }
+}
