d7300a3d4d9eedc31fc047eea1356ee8e4553184
Zenith08 <kaidenr@shaw.ca>
2022-09-18 13:34:25 -0700

    Title Screen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Base_Scene", LoadSceneMode.Single);
+        }
+    }
+}
