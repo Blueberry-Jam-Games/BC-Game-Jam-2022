63339c3cf0a33f5213e61ed34830a50d776ec910
Kaiden Richardson <kaidenr@shaw.ca>
2022-09-29 23:52:42 -0700

    Post jam upgrades that we wanted to fix.

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TitleScreen : MonoBehaviour
 {
+    public GameObject enterText;
+    private bool loading = false;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) && !loading)
         {
             SceneManager.LoadScene("Base_Scene", LoadSceneMode.Single);
+            loading = true;
+            enterText.SetActive(false);
         }
     }
 }
